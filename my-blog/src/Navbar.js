8e520c398b87d613406b4d2bc9cde044bedc85c3
Metyme1 <millionmety@gmail.com>
2023-10-14 16:15:28 +0300

    3rd

--- my-blog/src/Navbar.js
+++ my-blog/src/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
           <a href="/">Home</a>
           <a href="/create" style={{ 
             color: 'white', 
-            backgroundColor: '#f1356d',
+            backgroundColor: ' rgb(197, 108, 7)',
             borderRadius: '8px' 
           }}>New Blog</a>
         </div>

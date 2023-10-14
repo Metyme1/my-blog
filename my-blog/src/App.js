5fda8a42778fee98181dadc09d0f0c5070fb232c
Metyme1 <millionmety@gmail.com>
2023-10-14 10:27:22 +0300

    1st commit

---
+++ my-blog/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  const title = "hello"
+
+  const link ="http://www.google.com"
+
+  return (
+    <div className="App">
+     <h1>{title}</h1>
+     <a href = {link}>google </a>
+    </div>
+  );
+}
+
+export default App;

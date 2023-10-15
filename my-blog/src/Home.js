853af7afe6265e8581f2ba819457c41bd6dba33e
Metyme1 <millionmety@gmail.com>
2023-10-15 06:25:35 +0300

    5th commit

--- my-blog/src/Home.js
+++ my-blog/src/Home.js
@@ -1,25 +1,18 @@
-
 import { useState } from "react";
-
+import BlogList from "./BlogList";
 
 const Home = () => {
-  // let name = 'mario';
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    // name = 'luigi';
-    setName('luigi');
-    setAge(30);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
   );
 }
-   
-  export default Home;
+ 
+export default Home;

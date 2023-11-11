1e7717d707f36a3c085b461bc7853ab61cd7471b
Metyme1 <millionmety@gmail.com>
2023-11-11 22:36:10 +0300

    lesson 15

--- my-blog/src/Home.js
+++ my-blog/src/Home.js
@@ -6,7 +6,10 @@ const Home = () => {
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+  ]);
+
+  const [name, setName]= useState('mario');
+
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -16,7 +19,7 @@ const Home = () => {
   useEffect(() => {
     console.log('use effect ran');
     console.log(blogs);
-  })
+  },[name]);
 
   return (
     <div className="home">

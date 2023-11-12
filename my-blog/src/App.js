352735291c4ce74c518753ea8c51bf1093221295
Metyme1 <millionmety@gmail.com>
2023-11-12 14:35:36 +0300

    ExactMatch Route

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,25 +1,25 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import { BrowserRouter as Router,Route,Switch} from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Navbar /> 
-      {/* the navbar is constant for all page */}
-      <div className="content">
-         <switch>
-          <Route path ="/">
-            <Home/>
-
-          </Route>
-         </switch>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
     </Router>
-
   );
 }
 

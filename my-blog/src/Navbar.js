1c487cae37f28b2a45223b4187f4da23457653e7
Metyme1 <millionmety@gmail.com>
2023-11-12 14:41:07 +0300

    router link

--- my-blog/src/Navbar.js
+++ my-blog/src/Navbar.js
@@ -1,17 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-    return (
-      <nav className="navbar">
-        <h1>my Blog</h1>
-        <div className="links">
-          <a href="/">Home</a>
-          <a href="/create" style={{ 
-            color: 'white', 
-            backgroundColor: ' rgb(197, 108, 7)',
-            borderRadius: '8px' 
-          }}>New Blog</a>
-        </div>
-      </nav>
-    );
-  }
-   
-  export default Navbar;
+  return (
+    <nav className="navbar">
+      <h1>The Dojo Blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</Link>
+      </div>
+    </nav>
+  );
+}
+ 
+export default Navbar;
